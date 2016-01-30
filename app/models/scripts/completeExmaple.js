9ba16dacf83de0a6f684efa80b837be5d49b0a0d
mishuagopian <mishuagopian@gmail.com>
2016-01-30 16:37:39 -0300

    Updating db complete example

--- app/models/scripts/completeExmaple.js
+++ app/models/scripts/completeExmaple.js
@@ -1,4 +1,6 @@
-var orm = require('orm');
+var orm = require('orm'),
+    bookModel = require('./../book'),
+    userModel = require('./../user');
 
 var init = function () {
     orm.connect('postgres://michelagopian:@127.0.0.1:5432/books', function (err, db) {
@@ -6,26 +8,8 @@ var init = function () {
 
         console.log('Connected to db!');
 
-        var Book  = db.define('book', {
-            name      :     { type: 'text', required: true },
-            author    :     { type: 'text' },
-            publisher :     { type: 'text' },
-            price     :     { type: 'number' },
-            link      :     { type: 'text' },
-            year      :     { type: 'integer' }
-        });
-        var User = db.define('user', {
-            firstName   :   { type: 'text', required: true },
-            lastName    :   { type: 'text', required: true },
-            username    :   { type: 'text', required: true },
-            email       :   { type: 'text', required: true },
-            password    :   { type: 'text', required: true }
-        }, {
-            validations: {
-                username    :   orm.enforce.unique('username already taken!'),
-                email       :   orm.enforce.unique('email already taken!') 
-            }
-        });
+        var Book  = bookModel.getModel(orm, db);
+        var User = userModel.getModel(orm, db);
 
         // add the table to the database
         db.sync(function(err) { 
