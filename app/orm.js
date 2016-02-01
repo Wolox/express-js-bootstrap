dc89e8cee0d7218f10f43573485f33294f6d9df6
mishuagopian <mishuagopian@gmail.com>
2016-02-01 19:04:07 -0300

    Not reusing connection

--- app/orm.js
+++ app/orm.js
@@ -2,8 +2,6 @@ var orm = require('orm'),
     Book = require('./models/book'),
     User = require('./models/user');
 
-var connection = null;
-
 function setupModels(orm, db) {
     var book  = Book.getModel(orm, db);
     var user = User.getModel(orm, db);
@@ -12,12 +10,6 @@ function setupModels(orm, db) {
 exports.init = function (app) {
 
     app.use(function (req, res, next) {
-
-        if (connection) {
-            req.db = connection;
-            req.models = connection.models;
-            next();
-        }
 
         orm.connect('postgres://michelagopian:@127.0.0.1:5432/books', function (err, db) {
             if (err) {
