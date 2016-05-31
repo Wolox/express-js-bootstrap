1237d8287425baaff7effb15f473242a3e5b3599
Michel Agopian <mishuagopian@gmail.com>
2016-05-31 18:12:20 -0300

    Morgan logging

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
+    morgan = require('morgan'),
     config = require('./config/config').config,
     routes = require('./app/routes'),
     orm = require('./app/orm');
@@ -11,6 +12,10 @@ var init = function () {
     // Client must send "Content-Type: application/json" header
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+
+    morgan.token('req-params', function (req, res) { return req.params });
+    app.use(morgan('[:date[clf]] :remote-addr - Request ":method :url" with params: :req-params. '
+        + 'Response status: :status.'));
 
     orm.init(app);
 
