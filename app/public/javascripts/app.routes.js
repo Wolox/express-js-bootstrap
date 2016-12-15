6c7ff22a307eee6ac3e90403bf472b40c6482921
Michel Agopian <mishuagopian@gmail.com>
2016-12-15 10:29:20 -0300

    Adding another client view and README update

--- app/public/javascripts/app.routes.js
+++ app/public/javascripts/app.routes.js
@@ -4,18 +4,28 @@ angular.module('app-angular').config([
 
     // For any unmatched urls
     $urlRouterProvider.otherwise(($injector) => {
-      $injector.get('$state').go('default');
+      $injector.get('$state').go('client');
     });
 
     // Now set up the states
     $stateProvider
-      .state('default', {
-        url: '/default',
+      .state('client', {
+        url: '/client',
         views: {
           main: {
             templateUrl: 'client.html',
             controller: 'ClientController',
             controllerAs: 'clientCtrl'
+          }
+        }
+      })
+      .state('anotherClient', {
+        url: '/anotherClient',
+        views: {
+          main: {
+            templateUrl: 'anotherClient.html',
+            controller: 'AnotherClientController',
+            controllerAs: 'anotherClientCtrl'
           }
         }
       });
---
+++ app/public/javascripts/src/AnotherClientController.js
@@ -0,0 +1,7 @@
+angular.module('app-angular').controller('AnotherClientController', [
+  function () {
+
+    this.phrase = 'This jade is from another client view.';
+
+  }
+]);
