41faa2b4bc2109d78d2c469644bd4312e4365bc1
Michel Agopian <mishuagopian@gmail.com>
2016-12-26 21:42:15 -0300

    db url in one-line

--- app/orm.js
+++ app/orm.js
@@ -4,8 +4,7 @@ const orm = require('orm'),
   models = require('./models/models');
 
 const DB_URL = config.common.database.url ||
-                `postgres://${config.common.database.username}:${config.common.database.password}
-                @${config.common.database.host}:${config.common.database.port}/${config.common.database.database}`;
+                `postgres://${config.common.database.username}:${config.common.database.password}@${config.common.database.host}:${config.common.database.port}/${config.common.database.database}`; // eslint-disable-line max-len
 const dbModels = {};
 
 exports.init = (app) => {
