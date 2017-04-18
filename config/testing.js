573e6176787bc26d52cb7a9e686fc116fea4bf87
Gonzalo Escandarini <gonziesc@gmail.com>
2017-04-18 13:51:50 -0300

    changed testing url

--- config/testing.js
+++ config/testing.js
@@ -2,7 +2,7 @@ exports.config = {
   environment: 'testing',
   common: {
     database: {
-      url: process.env.NODE_API_DB_URL,
+      url: process.env.NODE_API_DB_URL_TEST,
       host: process.env.NODE_API_DB_HOST,
       port: process.env.NODE_API_DB_PORT,
       database: process.env.NODE_API_DB_NAME_TEST,
