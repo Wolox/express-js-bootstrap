a5129008e0a3952d0b85655c7efc20d2f01d0107
Mishu Agopian <mishuagopian@gmail.com>
2017-06-01 10:34:40 -0300

    Set default status code for unhandled errors

--- app/middlewares/errors.js
+++ app/middlewares/errors.js
@@ -1,6 +1,6 @@
 const errors = require('../errors');
 
 exports.handle = (error, req, res, next) => {
-  res.status(error.statusCode);
+  res.status(error.statusCode || 500);
   res.send({ error: error.message });
 };
