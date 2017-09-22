61101b203c753b141de27db45b72752751a5f1ff
MimoMoreno <mmorenolafon@gmail.com>
2017-09-22 12:51:38 -0300

    Adding .env file to gitignore

--- config/index.js
+++ config/index.js
@@ -1,5 +1,31 @@
-const ENVIRONMENT = process.env.NODE_ENV || 'development';
+const getConfigFile = () => {
+  return process.env.NODE_ENV === 'testing' ? './.env.test' : './.env';
+};
 
-const configFile = `./${ENVIRONMENT}`;
+const configFile = getConfigFile();
 
-module.exports = require(configFile).config;
+const config = require(configFile).config;
+
+config.common = {
+  database: {
+    url: process.env.NODE_API_DB_URL,
+    host: process.env.NODE_API_DB_HOST,
+    port: process.env.NODE_API_DB_PORT,
+    database: process.env.NODE_API_DB_NAME,
+    username: process.env.NODE_API_DB_USERNAME,
+    password: process.env.NODE_API_DB_PASSWORD
+  },
+  api: {
+    bodySizeLimit: process.env.API_BODY_SIZE_LIMIT,
+    parameterLimit: process.env.API_PARAMETER_LIMIT
+  },
+  session: {
+    header_name: 'authorization',
+    secret: process.env.NODE_API_SESSION_SECRET
+  },
+  rollbar: {
+    accessToken: process.env.ROLLBAR_ACCESS_TOKEN
+  }
+};
+
+module.exports = config;
--- config/testing.js
+++ config/testing.js
@@ -1,25 +1,4 @@
 exports.config = {
   environment: 'testing',
-  isTesting: true,
-  common: {
-    database: {
-      url: process.env.NODE_API_DB_URL_TEST,
-      host: process.env.NODE_API_DB_HOST,
-      port: process.env.NODE_API_DB_PORT,
-      database: process.env.NODE_API_DB_NAME_TEST,
-      username: process.env.NODE_API_DB_USERNAME,
-      password: process.env.NODE_API_DB_PASSWORD
-    },
-    api: {
-      bodySizeLimit: process.env.API_BODY_SIZE_LIMIT,
-      parameterLimit: process.env.API_PARAMETER_LIMIT
-    },
-    session: {
-      header_name: 'authorization',
-      secret: process.env.NODE_API_SESSION_SECRET
-    },
-    rollbar: {
-      accessToken: process.env.ROLLBAR_ACCESS_TOKEN
-    }
-  }
+  isTesting: true
 };
