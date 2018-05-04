7e8515b7686cd4b65a2bf3413ef98272e4daeaa2
Gonzalo Escandarini <gonziesc@gmail.com>
2018-05-04 18:25:51 -0300

    rollbacking env test change

--- config/testing.js
+++ config/testing.js
@@ -3,11 +3,7 @@ exports.config = {
   isTesting: true,
   common: {
     database: {
-      host: process.env.DB_HOST_TEST,
-      port: process.env.DB_PORT_TEST,
-      name: process.env.DB_NAME_TEST,
-      username: process.env.DB_USERNAME_TEST,
-      password: process.env.DB_PASSWORD_TEST
+      name: process.env.NODE_API_DB_NAME_TEST
     },
     session: {
       secret: 'some-super-secret'
