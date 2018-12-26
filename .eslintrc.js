847c4f1a9ff17d64fa02b84256d626ca6f05e766
Gonzalo Escandarani <gonziesc@gmail.com>
2018-12-26 11:30:45 -0300

    adding callbacks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,3 @@
 const {merge} = require('lodash'),
  eslintConfig = require('eslint-config-wolox');
-module.exports = eslintConfig;
-
-// merge(eslintConfig, {rules: {"max-nested-callbacks": ["error", { max: 12 }]}})
-
-/*
-  ...{
-    "parser": "babel-eslint",          // https://github.com/babel/babel-eslint
-    "parserOptions": {
-    "sourceType": "module"
-    },
-    "env": {                           // http://eslint.org/docs/user-guide/configuring.html#specifying-environments
-      "node": true,                    // node global variables
-      "mocha": true,                   // mocha keywords
-      "es6": true
-    }
-  }});
-  */
+module.exports = merge(eslintConfig, {rules: {"max-nested-callbacks": ["error", { max: 12 }]}})
