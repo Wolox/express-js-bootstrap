760a408a44b12cfe7c90689d7990a494f239cec6
andrea <andrea.celtich@wolox.com.ar>
2019-07-16 16:55:15 -0300

    implemented api information of node, date, version and dependencies on differents endpoints

---
+++ generators/app/templates/app/middlewares/apiInfo.js
@@ -0,0 +1,16 @@
+const { merge } = require('lodash');
+const pjson = require('../../package.json');
+
+exports.apiInformation = (req, res, next) => {
+  try {
+    if (!res.headers) {
+      res.headers = {};
+    }
+    merge(res.headers, { 'X-API-Date': new Date() });
+    merge(res.headers, { 'X-Package-Version': pjson.version });
+    merge(res.headers, { 'X-Node-Version': pjson.engines.node });
+    return next();
+  } catch (err) {
+    return res.status(400).send('There was a problem to get API info');
+  }
+};
