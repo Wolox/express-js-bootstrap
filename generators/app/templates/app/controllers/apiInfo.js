4eda288377ac8ed4faf885b6419f777289966f1b
andrea <andrea.celtich@wolox.com.ar>
2019-07-23 14:21:53 -0300

    replaced apiInfo middleware by its controller

--- generators/app/templates/app/controllers/apiInfo.js
+++ generators/app/templates/app/controllers/apiInfo.js
@@ -1,5 +1,14 @@
+const { merge } = require('lodash');
+const pjson = require('../../package.json');
+
 exports.apiInfo = (req, res, next) => {
   try {
+    if (!req.body) {
+      req.body = {};
+    }
+    merge(req.body, { apiDate: new Date() });
+    merge(req.body, { packageVersion: pjson.version });
+    merge(req.body, { nodeVersion: pjson.engines.node });
     return res.status(200).send(req.body);
   } catch (err) {
     return next(err);
