97f2a17082c6d7f75accf618bfa30619219302ee
andrea <andrea.celtich@wolox.com.ar>
2019-07-23 15:15:26 -0300

    added headers and deleted merge

--- generators/app/templates/app/middlewares/apiInfo.js
+++ generators/app/templates/app/middlewares/apiInfo.js
@@ -1,4 +1,3 @@
-const { merge } = require('lodash');
 const pjson = require('../../package.json');
 
 exports.apiInformation = (req, res, next) => {
@@ -6,9 +5,9 @@ exports.apiInformation = (req, res, next) => {
     if (!res.headers) {
       res.headers = {};
     }
-    merge(res.headers, { 'X-API-Date': new Date() });
-    merge(res.headers, { 'X-Package-Version': pjson.version });
-    merge(res.headers, { 'X-Node-Version': pjson.engines.node });
+    res.headers['X-API-Date'] = new Date();
+    res.headers['X-Package-Version'] = pjson.version;
+    res.headers['X-Node-Version'] = pjson.engines.node;
     return next();
   } catch (err) {
     return next(err);
