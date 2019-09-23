b18833eac6db7b0cede68a4a4d22b750e9fe635d
andrea <andrea.celtich@wolox.com.ar>
2019-09-23 17:01:19 -0300

    added factory girl with mongo db

--- generators/app/constants.js
+++ generators/app/constants.js
@@ -95,7 +95,7 @@ exports.files = [
   {
     directory: 'test/factory',
     name: 'factory_by_models.ejs',
-    condition: answers => answers.orm && answers.orm.sequelize && answers.testing === 'jest-supertest'
+    condition: answers => answers.orm && answers.testing === 'jest-supertest'
   },
   {
     directory: 'test',
