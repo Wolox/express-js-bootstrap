b1022836ea6c8eabc4ce5ea9fc473422b342aed8
raparicio6 <rodrigoaparicio6@gmail.com>
2019-09-24 15:32:03 -0300

    adding curly brackets as new rule demands

--- generators/app/templates/migrations/index.js
+++ generators/app/templates/migrations/index.js
@@ -22,8 +22,9 @@ exports.check = () => {
   });
   return umzug.pending().then(migrations => {
     if (migrations.length) {
-      if (!config.isProduction)
+      if (!config.isProduction) {
         return Promise.reject(new Error('Pending migrations, run: npm run migrations'));
+      }
 
       return umzug.up().catch(err => {
         logger.error(err);
