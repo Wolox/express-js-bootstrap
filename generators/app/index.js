1396dd91a1c34261bdc726a6e7e703f70840667a
YesicaNavaB <yesica.betancourt@wolox.co>
2019-10-17 12:55:08 -0500

    add log to validate the route in the travis

--- generators/app/index.js
+++ generators/app/index.js
@@ -60,6 +60,8 @@ const nodeGenerator = class extends Generator {
   _copyTplPromise(templatePath, filePath, options) {
     return new Promise((resolve, reject) => {
       try {
+        this.log(this.templatePath(`${this.answers.technology}/${templatePath}`));
+        console.log(this.templatePath(`${this.answers.technology}/${templatePath}`));
         this.fs.copyTpl(
           this.templatePath(`${this.answers.technology}/${templatePath}`),
           this._destinationPath(filePath),
