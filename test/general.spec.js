0f60801c0a1c61330dfb72fe4666c56c25951bd3
YesicaNavaB <yesica.betancourt@wolox.co>
2019-10-18 09:24:59 -0500

    add general graphql test

--- test/general.spec.js
+++ test/general.spec.js
@@ -1,32 +1,61 @@
 const { flatten } = require('lodash'),
   utils = require('./helpers/utils'),
   { mockCommand } = require('./helpers/mocks'),
-  { exampleProjects, basicFiles } = require('./helpers/constants');
+  { exampleProjectsGraphql } = require('./helpers/projects'),
+  { exampleProjects, basicFiles, basicFilesGraphql } = require('./helpers/constants');
 
 beforeAll(() => mockCommand());
 
-const testSnapshot = technology => (projectName, { kickoffOptions, files, templateFiles }) => {
-  beforeAll(() =>
-    utils.runKickoff({
-      ...kickoffOptions,
-      nodeVersion: '8.9.12',
-      technology,
-      npmVersion: '6.4.1',
-      inTraining: false,
-      projectName: 'Project',
-      projectDescription: 'Project',
-      urlRepository: 'https://test.com.ar'
-    })
-  );
+describe.each(exampleProjects)(
+  'Example project with %s',
+  (projectName, { kickoffOptions, files, templateFiles }) => {
+    beforeAll(() =>
+      utils.runKickoff({
+        ...kickoffOptions,
+        nodeVersion: '8.9.12',
+        technology: 'nodeJS',
+        npmVersion: '6.4.1',
+        inTraining: false,
+        projectName: 'Project',
+        projectDescription: 'Project',
+        urlRepository: 'https://test.com.ar'
+      })
+    );
 
-  test(`creates basic files for ${projectName}`, () => {
-    utils.checkExistentFiles(files, 'Project');
-    utils.checkExistentFiles(basicFiles, 'Project');
-  });
+    test(`creates basic files for ${projectName}`, () => {
+      utils.checkExistentFiles(files, 'Project');
+      utils.checkExistentFiles(basicFiles, 'Project');
+    });
 
-  test.each(flatten([...templateFiles, ...basicFiles]))('creates expected %s', file => {
-    expect(utils.getFileContent(`Project/${file}`)).toMatchSnapshot();
-  });
-};
+    test.each(flatten([...templateFiles, ...basicFiles]))('creates expected %s', file => {
+      expect(utils.getFileContent(`Project/${file}`)).toMatchSnapshot();
+    });
+  }
+);
 
-describe.each(exampleProjects)('Example project with %s', testSnapshot('nodeJS'));
+describe.each(exampleProjectsGraphql)(
+  'Example project with %s',
+  (projectName, { kickoffOptions, files, templateFiles }) => {
+    beforeAll(() =>
+      utils.runKickoff({
+        ...kickoffOptions,
+        nodeVersion: '8.9.12',
+        technology: 'graphQL',
+        npmVersion: '6.4.1',
+        inTraining: false,
+        projectName: 'Project',
+        projectDescription: 'Project',
+        urlRepository: 'https://test.com.ar'
+      })
+    );
+
+    test(`creates basic files for ${projectName}`, () => {
+      utils.checkExistentFiles(files, 'Project');
+      utils.checkExistentFiles(basicFilesGraphql, 'Project');
+    });
+
+    test.each(flatten([...templateFiles, ...basicFilesGraphql]))('creates expected %s', file => {
+      expect(utils.getFileContent(`Project/${file}`)).toMatchSnapshot();
+    });
+  }
+);
