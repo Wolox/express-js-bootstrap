ee3439b626d68863fa56953ea91e505ec01a1f2f
YesicaNavaB <yesica.betancourt@wolox.co>
2019-10-18 09:04:17 -0500

    delete test general

--- test/general.spec.js
+++ test/general.spec.js
@@ -1,8 +1,7 @@
 const { flatten } = require('lodash'),
   utils = require('./helpers/utils'),
   { mockCommand } = require('./helpers/mocks'),
-  { exampleProjectsGraphql } = require('./helpers/projects'),
-  { exampleProjects, basicFiles, basicFilesGraphql } = require('./helpers/constants');
+  { exampleProjects, basicFiles } = require('./helpers/constants');
 
 beforeAll(() => mockCommand());
 
@@ -29,28 +28,5 @@ const testSnapshot = technology => (projectName, { kickoffOptions, files, templa
     expect(utils.getFileContent(`Project/${file}`)).toMatchSnapshot();
   });
 };
-const testSnapshotGraph = technology => (projectName, { kickoffOptions, files, templateFiles }) => {
-  beforeAll(() =>
-    utils.runKickoff({
-      ...kickoffOptions,
-      nodeVersion: '8.9.12',
-      technology,
-      npmVersion: '6.4.1',
-      inTraining: false,
-      projectName: 'Project',
-      projectDescription: 'Project',
-      urlRepository: 'https://test.com.ar'
-    })
-  );
 
-  test(`creates basic files for ${projectName}`, () => {
-    utils.checkExistentFiles(files, 'Project');
-    utils.checkExistentFiles(basicFilesGraphql, 'Project');
-  });
-
-  test.each(flatten([...templateFiles, ...basicFilesGraphql]))('creates expected %s', file => {
-    expect(utils.getFileContent(`Project/${file}`)).toMatchSnapshot();
-  });
-};
 describe.each(exampleProjects)('Example project with %s', testSnapshot('nodeJS'));
-describe.each(exampleProjectsGraphql)('Example project with %s', testSnapshotGraph('graphQL'));
