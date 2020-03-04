c20d204a6c434b991dcfce6378427dcc85db0e36
julian molina <molina.bohemio@gmail.com>
2020-03-04 11:31:03 -0300

    run linter test with our eslint configuration

--- test/linter.spec.js
+++ test/linter.spec.js
@@ -1,11 +1,19 @@
-const { inspect } = require('util');
+const path = require('path');
 
 const utils = require('./helpers/utils');
 const { mockCommand } = require('./helpers/mocks');
 const { runCommand } = require('../generators/app/command');
 const { exampleProjects } = require('./helpers/constants');
 
-const commands = utils.getLinterCommands();
+const commands = [
+  {
+    description: 'Lint fix',
+    name: 'npm',
+    args: ['run', 'lint-external', '--', '--fix', '--no-eslintrc']
+  },
+  { description: 'Lint', name: 'npm', args: ['run', 'lint-external', '--', '--no-eslintrc'] }
+];
+const actualDirectory = path.resolve(__dirname);
 
 describe.each(exampleProjects)('Example project with %s', (projectName, { kickoffOptions }) => {
   beforeAll(() => {
@@ -15,19 +23,25 @@ describe.each(exampleProjects)('Example project with %s', (projectName, { kickof
       nodeVersion: '8.9.12',
       npmVersion: '6.4.1',
       inTraining: false,
-      projectName: 'linterTest',
+      projectName: 'linter',
       projectDescription: 'Fake project to test linter',
       urlRepository: 'https://test.com.ar'
     });
   });
 
   test.each(commands)('run the EsLinter for each project generated', command => {
-    jest.setTimeout(15000);
-    const directory = utils.getTestDirectory('linterTest');
-    return runCommand({ ...command, spawnOptions: { cwd: directory } }).catch(err => {
-      throw new Error(
-        `Eslinter for project failed on command: ${inspect(command)} with error: ${inspect(err)}`
-      );
+    const directory = utils.getTestDirectory('linter');
+    const args = [...command.args, `${directory}`];
+    const projectRootDirectory = actualDirectory
+      .split('/')
+      .slice(0, -1)
+      .join('/');
+    return runCommand({
+      ...command,
+      args,
+      spawnOptions: { cwd: projectRootDirectory }
+    }).catch(err => {
+      throw new Error(`Eslinter for project failed on command: ${command.description} with error: ${err}`);
     });
   });
 });
