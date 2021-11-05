0dcd68b8717f30e9841215e28c060320a2479fe3
Daniel Benavides <49955938+dbenavidesv@users.noreply.github.com>
2021-11-05 12:14:21 -0500

    fix: fixing sequelize model imports, due to sequelize version upgrade (#549)
    Co-authored-by: Daniel <daniel.benavides@wolox.com>

--- generators/app/templates/app/models/index.js
+++ generators/app/templates/app/models/index.js
@@ -11,7 +11,8 @@ const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.p
 fs.readdirSync(__dirname)
   .filter(file => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js')
   .forEach(file => {
-    const model = sequelize.import(path.join(__dirname, file));
+    // eslint-disable-next-line global-require
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
